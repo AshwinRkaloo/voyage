2f6c782a0f46e3d6409686139c06875c687e3420
ashwin <ashwin@nleurope.com>
2019-02-07 16:26:39 +0400

    Added bxslider

--- js/myjs.js
+++ js/myjs.js
@@ -19,3 +19,34 @@ $(function(){
         return false; 
     }); 
 });
+
+/* BX SLIDER - TESTIMONIALS */
+$(function(){
+    $('.slider-testimonials').bxSlider({
+        controls : true,
+        auto: true,
+        speed: 500,
+        pause: 2000,
+        slideWidth: 475,
+        maxSlides: 5,
+        slideMargin: 20,
+        moveSlides: 1,
+        pager: false,
+
+        /* ADD CLASS ACTIVE TO CURRENT SLIDE */
+        onSlideAfter: function (currentSlideNumber, totalSlideQty, currentSlideHtmlObject) {
+            // console.log(currentSlideHtmlObject);
+            // $('.active').removeClass('active');
+            $('.slider-testimonials > .slides').eq(currentSlideHtmlObject + 1).addClass('active')
+        },
+        // onSliderLoad: function () {
+        //     $('.slider-testimonials > .slides').eq(1).addClass('active')
+        // }   
+    })
+});
+
+
+
+
+
+
